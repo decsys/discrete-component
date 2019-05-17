19708026878743a3e927f8060b4ace6ca911b6e4
Jonathan Couldridge <jcouldridge@outlook.com>
2019-05-17 15:49:06 +0100

    dependency tidy up

--- rollup.config.js
+++ rollup.config.js
@@ -27,10 +27,17 @@ export default {
       react: "React",
       "styled-components": "styled",
       "react-dom": "ReactDOM",
-      "prop-types": "PropTypes"
+      "prop-types": "PropTypes",
+      victory: "Victory"
     }
   },
-  external: ["react", "styled-components", "react-dom", "prop-types"],
+  external: [
+    "react",
+    "styled-components",
+    "react-dom",
+    "prop-types",
+    "victory"
+  ],
   plugins: [
     replace({
       "process.env.NODE_ENV": JSON.stringify("production")
