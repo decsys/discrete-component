3369c1cd1e14c064b17b9f1e854aba5c6b8f7858
Jonathan Couldridge <jonathan.couldridge@nottingham.ac.uk>
2019-06-05 09:42:43 +0100

    fixing params, allow either primary or secondary labels

--- src/Component.js
+++ src/Component.js
@@ -37,11 +37,12 @@ const Component = ({
     .reduce((acc, key) => {
       if (key.includes("Secondary")) return acc; // ignore secondary params
 
-      if (!radioParams[key]) return acc;
-      
-      const radio = [radioParams[key]]; // add the radio value
-      if (radioParams[`${key}Secondary`])
-        radio.push(radioParams[`${key}Secondary`]); // add the Secondary label
+      const secondaryKey = `${key}Secondary`;
+
+      if (!radioParams[key] && !radioParams[secondaryKey]) return acc;
+
+      const radio = [radioParams[key], radioParams[secondaryKey]]; // add the radio value
+      //if (radioParams[secondaryKey]) radio.push(radioParams[secondaryKey]); // add the Secondary label
 
       acc.push(radio);
       return acc;
